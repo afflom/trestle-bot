8f84bea02a2efbef33a977a8a8226134cf3cccaa
Alex Flom <alexander.flom@gmail.com>
2023-06-28 14:55:24 -0400

    troubleshooting git error1

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ cat log.txt
 
 commit=$(echo "$output" | grep "Commit Hash:" | sed 's/.*: //')
 
-echo "${ls -la}"
+echo ls -la
 
 if [ -n "$commit" ]; then
     echo "changes=true" >> "$GITHUB_OUTPUT"
